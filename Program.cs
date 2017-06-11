645f50274063cfeb95ec4013f87cd7da36514aa9
justBanks <Darryl.Banks@gmail.com>
2017-06-11 03:08:08 -0700

    configure dev mode (hosting.json)

--- Program.cs
+++ Program.cs
@@ -12,8 +12,14 @@ namespace aspnetcore_reactredux
     {
         public static void Main(string[] args)
         {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("hosting.json", optional: true)
+                .Build();
+
             var host = new WebHostBuilder()
                 .UseKestrel()
+                .UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
