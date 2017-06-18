90e0bfa0a833109c00ecfcb2d630bd4a13597262
justBanks <Darryl.Banks@gmail.com>
2017-06-18 01:09:32 -0700

    gitignore => hosting.json file

--- Program.cs
+++ Program.cs
@@ -12,14 +12,8 @@ namespace aspnetcore_reactredux
     {
         public static void Main(string[] args)
         {
-            var config = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("hosting.json", optional: true)
-                .Build();
-
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
                 .UseIISIntegration()
                 .UseStartup<Startup>()
